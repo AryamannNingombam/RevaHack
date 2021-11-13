21211499fb3ec236ea2efe3d9221ba608b484edd
ary_amann <aryamannsingh9@gmail.com>
2021-11-13 12:12:03 +0530

    f

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://mrex-backend.herokuapp.com/";
+export const BACKEND_URL = "https://3abf-2401-4900-4bb0-4680-6085-8df3-7793-6719.ngrok.io";
 export const BACKGROUND_COLOR = "#3DBBF1";
 export const BACKGROUND_WHITE_COLOR = "#F5F5F5";
 export const SECONDARY_FONT = "BasisGrotesqueProMiddle";
